19d50b0b1a514b37735c216936e97be3936f0a3d
MariusGhiniei <marius_iulian_23@yahoo.com>
2022-04-29 06:10:13 +0300

    footer UI

---
+++ footer/footer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zuzugram</title>
+
+    <!-- Bulma import -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    
+    <!-- icon -->
+    <link rel="icon" href ="../header/assets/cat.png">
+</head>
+<body>
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>Zuzugram</strong> 
+          by <a href="https://www.linkedin.com/in/marius-ghiniei-441654160/">
+          Ghiniei Marius - Iulian</a> <br>   &copy; 2022 <br>
+         <em> A college project. </em>
+    </p>
+  </div>
+</footer>
+</body>
+</html>

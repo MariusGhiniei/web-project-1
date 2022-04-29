2478e2f91dea5865d78165555a6cc89460ebb153
MariusGhiniei <marius_iulian_23@yahoo.com>
2022-04-29 06:21:32 +0300

    removed head content

--- footer/footer.php
+++ footer/footer.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zuzugram</title>
 
-    <!-- Bulma import -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
-    
-    <!-- icon -->
-    <link rel="icon" href ="../header/assets/cat.png">
-</head>
-<body>
 <footer class="footer">
   <div class="content has-text-centered">
     <p>
